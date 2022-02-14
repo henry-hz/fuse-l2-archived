ac3b36119b6e6cf8a038a4e658b83d91539b481f
Henry Hazan <henry@teramine.io>
2022-02-14 17:16:30 +0200

    add multiple compilers

--- bridge/hardhat.config.ts
+++ bridge/hardhat.config.ts
@@ -23,7 +23,28 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.6.11",
+  solidity: {
+    compilers: [
+      {
+        version: "0.6.11",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+        },
+      },
+      {
+        version: "0.8.7",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 100,
+          },
+        },
+      },
+    ],
+  },
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
