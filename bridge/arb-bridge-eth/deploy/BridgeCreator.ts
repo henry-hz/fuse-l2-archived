f6730e1e23c9862f5061fd0e6a1cbae987841916
Ubuntu <ubuntu@ip-172-31-11-183.eu-central-1.compute.internal>
2022-02-17 16:34:51 +0000

    wip

--- bridge/arb-bridge-eth/deploy/BridgeCreator.ts
+++ bridge/arb-bridge-eth/deploy/BridgeCreator.ts
@@ -2,16 +2,15 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
-  const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
-  const { deployer } = await getNamedAccounts();
-  await deploy("BridgeCreator", {
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
+  const { deployer } = await getNamedAccounts()
+ 
+  await deploy('BridgeCreator', {
     from: deployer,
     args: [],
   })
 }
 
-export default func;
-func.tags = ["BridgeCreator", "live"];
-//module.exports = func
-//module.exports.tags = ['BridgeCreator', 'live']
+module.exports = func
+module.exports.tags = ['BridgeCreator', 'live']
