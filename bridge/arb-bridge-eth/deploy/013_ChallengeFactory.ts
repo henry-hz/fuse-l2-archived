70be479584ad184110a450a2eb9133aa355321b0
Henry Hazan <henry@teramine.io>
2022-02-19 20:36:25 +0200

    add type

--- bridge/arb-bridge-eth/deploy/013_ChallengeFactory.ts
+++ bridge/arb-bridge-eth/deploy/013_ChallengeFactory.ts
@@ -1,5 +1,5 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { DeployFunction } from 'hardhat-deploy/types';
+import { DeployFunction, DeployResult } from "hardhat-deploy/types";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { deployments, getNamedAccounts } = hre;
