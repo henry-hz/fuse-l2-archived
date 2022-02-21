6a617c3be517696f9868d70a0fbf657bd15b601c
Ubuntu <ubuntu@ip-172-31-11-183.eu-central-1.compute.internal>
2022-02-21 16:30:54 +0000

    add make builders for arb-node-core

---
+++ validator/arb-avm-cpp/cmachine/flags.go
@@ -0,0 +1,8 @@
+package cmachine
+
+/*
+#cgo CFLAGS: -I. -I../data_storage/include -fno-omit-frame-pointer
+#cgo LDFLAGS:  -L/home/ubuntu/fuse-arb/validator/arb-avm-cpp/release/lib -L/home/ubuntu/.hunter/_Base/1377523/1720112/1f8c6e1/Install/lib /home/ubuntu/fuse-arb/validator/arb-avm-cpp/release/lib/libcavm.a /home/ubuntu/fuse-arb/validator/arb-avm-cpp/release/lib/libdata_storage.a /home/ubuntu/fuse-arb/validator/arb-avm-cpp/release/lib/libavm.a /home/ubuntu/fuse-arb/validator/arb-avm-cpp/release/lib/libavm_values.a /home/ubuntu/fuse-arb/validator/arb-avm-cpp/release/lib/libff.a /home/ubuntu/fuse-arb/validator/arb-avm-cpp/release/lib/libsecp256k1.a /usr/lib/x86_64-linux-gnu/libcrypto.so /usr/local/lib/librocksdb.so /home/ubuntu/.hunter/_Base/1377523/1720112/1f8c6e1/Install/lib/libkeccak.a   /usr/lib/x86_64-linux-gnu/libgmp.so /usr/lib/x86_64-linux-gnu/libgmpxx.so -lstdc++ -lm -ldl
+#cgo linux LDFLAGS: -latomic
+*/
+import "C"
